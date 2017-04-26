b954d939ace3a9e5061bbda0774d86a305adee46
ParmuSingh <p.s.obheroi@gmail.com>
2017-04-26 19:52:41 +0530

    Removed useless comments
    In KNeighboursClassifier.java

--- KNeighboursClassifier.java
+++ KNeighboursClassifier.java
@@ -16,7 +16,7 @@ public class KNeighboursClassifier {
 				featureSpace = new int[i][j];
 			labels = new int[i];
 		}
-				//System.out.println(featureSpace.length);
+				
 	}
 	public KNeighboursClassifier()
 	{
@@ -31,13 +31,6 @@ public class KNeighboursClassifier {
 	}
 	public void fit(int featureSpaceT[][], int labelsT[])// Applicable for single valued features only.
 	{
-		//noOfTuples = featureSpaceT.length;
-		//noOfFeatures = noOfFeaturesT;
-		//featureSpace[][] = new int[noOfTuples][noOfFeatures];
-		//labels[]= new int[noOfTuples];
-		//noOfTuples = 10;
-		//noOfFeatures = 20;
-		//System.out.println(featureSpace.length);
 		
 		/*
 		 * 
@@ -63,16 +56,7 @@ public class KNeighboursClassifier {
 				featureSpace[i][j] = featureSpaceT[i][j];
 				labels[i] = labelsT[i];
 			}
-		//BEKAR KA DISPLAY.
-		/*
-		System.out.println("Training data:\nf f f f f f f f f f f f f f f f f f f f L");
-		for(int i=0; i<noOfTuples; i++)
-		{
-			for(int j=0; j<noOfFeatures; j++)
-				System.out.print(featureSpace[i][j] + " ");
-			System.out.println(labels[i]);
-		}
-		*/		
+		
 	}
 	
 	public int predict(int testFeatures[]) throws ArrayIndexOutOfBoundsException
@@ -85,12 +69,8 @@ public class KNeighboursClassifier {
 		int indices[] = new int[noOfTuples];
 		for(int i=0; i< noOfTuples; i++)
 			indices[i] = i;
-		/*
-		System.out.println("Testing data:");
-		for(int i=0; i< noOfFeatures; i++)
-			System.out.print(testFeatures[i] + " ");
-		System.out.println();
-		*/
+		
+		
 		//FINDING DIFFERNCES
 		for(int i=0; i< noOfTuples; i++)
 		{
@@ -100,9 +80,8 @@ public class KNeighboursClassifier {
 				 sum+=((testFeatures[j] - featureSpace[i][j])*(testFeatures[j] - featureSpace[i][j]));
 			}
 			distance[i] = Math.sqrt(sum);
-			//System.out.print(distance[i] + " ");
 		}
-		//System.out.println();
+		
 		
 		//SORTING | It's currently bubble sort. I'll change to something more efficient like quick sort.
 		for(int i=0; i< noOfTuples-1; i++)
@@ -116,37 +95,10 @@ public class KNeighboursClassifier {
 					indices[j] = indices[i] - indices[j];
 					indices[i] = indices[i] - indices[j];
 				}
-		/*
-		System.out.print("SORTED: ");
-		for(int i=0 ;i<distance.length; i++)
-			System.out.print(distance[i] + " ");
-		System.out.println();
-		*/
+		
 		//PREDICTING
 		int prediction = labels[indices[0]];
 		
 		return prediction;
 	}
-	/*
-	public static void main(String args[])
-	{
-		int features[][] = new int[10][20],//[noOfTuples][noOfFeatures] 
-				labels[] = new int[10];//[Single Attribute]
-		int test[] = new int[20];
-		for(int i=0; i< 10; i++)
-		{
-			for(int j=0; j<20; j++)
-			{
-				features[i][j] = i;
-				test[j] = 1;
-			}
-			labels[i] = 9-i;			
-		}
-		int noOfFeatures = 20;
-		//System.out.println(features.length);
-		
-		KNeighboursClassifier KNC = new KNeighboursClassifier(features.length, noOfFeatures);
-		KNC.fit(features, labels);
-		System.out.println("The prediction is " + KNC.predict(test));
-	}*/
 }
