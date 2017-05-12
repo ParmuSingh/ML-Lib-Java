952485f21698add344dc418130a6c3ff891067ec
Paramdeep Singh Obheroi <p.s.obheroi@gmail.com>
2017-05-12 16:01:13 +0530

    Removed useless comments

--- NeuralNetwork.java
+++ NeuralNetwork.java
@@ -2,25 +2,14 @@ import javax.sql.rowset.spi.SyncFactory;
 
 public class NeuralNetwork {
 	
-//	private int noOfExamples, noOfFeatures;
-//	private int featureSpace[][];
-//	private int labels[];
 	private double synapticWeights[][];
 
 	public NeuralNetwork(int numberOfFeatures)
 	{
 		for(int i=0; i< numberOfFeatures; i++)
-		{
 			for(int j=0; j<=1; j++)
-				synapticWeights = new double[i][j];
-			//System.out.println(i + ": " + synapticWeights[i][0]);
-//			synapticWeights[i][0] = 1.0;
-		}
-		
-		
+				synapticWeights = new double[i][j];		
 	}
-	
-	
 	
 	private double[][] addMatrices(double x[][], double y[][])
 	{
@@ -45,8 +34,6 @@ public class NeuralNetwork {
 		for(int i=0; i<z.length; i++)
 			for(int j=0; j<z[0].length; j++)
 				z[i][j] = x[i][j] - y[i][j];
-		System.out.println("subtracted inputLabels - output");
-		print(z);
 		return z;
 	}
 	
@@ -56,9 +43,7 @@ public class NeuralNetwork {
 		for(int i=0; i<=x[0].length; i++)
 			for(int j=0; j<=x.length; j++)
 				z = new double[i][j];
-//		print(z);
-//		System.out.println(x.length);
-//		System.out.println(x[0].length);
+
 		for(int i=0; i<x.length; i++)
 			for(int j=0; j<x[0].length; j++)
 				z[j][i] = x[i][j];
@@ -72,28 +57,18 @@ public class NeuralNetwork {
 			for(int j=0; j<=y[0].length; j++)
 				z = new double[i][j];
 		
-//		print(x);
-//		print(y);
-//		System.out.println(x[0].length);
-//		System.out.println(y.length);
-//		System.out.println(synapticWeights.length);
-//		System.out.println(synapticWeights[0].length);
-//		System.out.println(z.length);
-//		System.out.println(x.length);
-		
 		double sum = 0.0;
-		for (int i = 0 ; i < x.length ; i++ )
-        {
-           for ( int j=0; j < y[0].length; j++)
-           {   
-              for (int k=0; k < x[0].length; k++ )
-                 sum += x[i][k]*y[k][j];
-              z[i][j] = sum;
-              sum = 0;
-           }
-        }
-//		print(z);
 		
+		for (int i = 0 ; i < x.length ; i++ )
+        	{
+           		for ( int j=0; j < y[0].length; j++)
+           		{   
+              			for (int k=0; k < x[0].length; k++ )
+                 			sum += x[i][k]*y[k][j];
+              			z[i][j] = sum;
+              			sum = 0;
+           		}
+        	}
 		return z;
 	}
 	
@@ -102,8 +77,6 @@ public class NeuralNetwork {
 		for(int i=0; i<z.length; i++)
 			for(int j=0; j<z[0].length; j++)
 				z[i][j] = (1/(1 + Math.exp(-z[i][j])));
-//		System.out.println(z);
-//		print(z);
 		return z;
 	}
 	
@@ -120,40 +93,27 @@ public class NeuralNetwork {
 	
 	public void train(double inputFeatures[][], double inputLabels[][], int iterations)
 	{
-//		print(inputFeatures);
-//		print(inputLabels);
-//		System.out.print(inputFeatures[0].length);
+		
 		for(int i=0; i< inputFeatures[0].length; i++)
-		{
-//			synapticWeights = new double[i][0];
 			for(int j=0; j<1; j++)
 				synapticWeights[i][j] = 1.0;
-		}
-		synapticWeights[0][0] = -0.16595599;
-		synapticWeights[1][0] = 0.44064899;
-		synapticWeights[2][0] = -0.99977125;
+// 		synapticWeights[0][0] = -0.16595599;
+// 		synapticWeights[1][0] = 0.44064899;
+// 		synapticWeights[2][0] = -0.99977125;
 		
 		double output[][] = null;
 		double error[][] = null;
 		double adjustments[][] = null;
-		System.out.println("Initial Synaptic weights:");
-		print(synapticWeights);
 		
 		for(int i=0; i<iterations; i++)
 		{
 			output = predict(inputFeatures);
-			System.out.println("output:");
-			print(output);
+			
 			error = subtractMatrices(inputLabels, output);
-			System.out.println("Error @ i = " + i + ": ");
-			print(error);
-			
+	
 			adjustments = dotProduct(transpose(inputFeatures), error);
-			System.out.println("Adjusts:");
-			print(adjustments);
-			synapticWeights = addMatrices(synapticWeights, adjustments);
-			print(synapticWeights);
 			
+			synapticWeights = addMatrices(synapticWeights, adjustments);
 		}
 		
 	}
